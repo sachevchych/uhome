5c0794b83a7c807373c8d1c93d199298be7a43c9
Artem Sachevchych <Sachevchych@gmail.com>
2020-12-29 15:54:03 +0200

    Edited keys

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://admin:UB16nUoV3edkeyxd@uhome.wcfgb.mongodb.net/uHome?retryWrites=true&w=majority',
+  MONGO_URI: 'mongodb+srv://admin:8OwgxWVfYfDfXN3t@uhome.wcfgb.mongodb.net/uHome?retryWrites=true&w=majority',
   JWT: 'dev-jwt-key'
 };
--- server/keys/keys.prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGO_URI: 'mongodb+srv://admin:UB16nUoV3edkeyxd@uhome.wcfgb.mongodb.net/uHome?retryWrites=true&w=majority',
-  JWT: 'dev-jwt-key'
-};
